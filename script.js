ecfb6b7fc993c77b36e59a9b11ed1eb8aa6e01f8
RicardoAlcantar25 <ricardoac.ti23@utsjr.edu.mx>
2025-01-14 18:35:33 -0600

    App de citas medicas

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-// Referencias a elementos del DOM
 const form = document.getElementById('appointmentForm');
 const list = document.getElementById('list');
 
-// Manejar el envío del formulario
 form.addEventListener('submit', function (event) {
-    event.preventDefault(); // Evitar el envío por defecto
+    event.preventDefault(); 
 
-    // Obtener valores de los campos del formulario
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const specialty = document.getElementById('specialty').value;
     const date = document.getElementById('date').value;
     const time = document.getElementById('time').value;
 
-    // Crear un elemento de lista con los datos
+    
     const listItem = document.createElement('li');
     listItem.textContent = "Nombre: " + name + ", Correo: " + email + ", Especialidad: " + specialty + ", Fecha: " + date + ", Hora: " + time;
-    // Agregar el elemento a la lista de citas
+    
     list.appendChild(listItem);
 
-    // Reiniciar el formulario
+    
     form.reset();
 });
